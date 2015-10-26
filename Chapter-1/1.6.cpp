58b502ab252baaebd1a6e400da7a437678187d44
Cole Hudson <Cole-Hudson@users.noreply.github.com>
2015-10-26 15:37:49 -0700

    Create 1.6.cpp

---
+++ Chapter-1/1.6.cpp
@@ -0,0 +1,7 @@
+//1.6 
+
+/*
+This is really straightforward if you have any issues with this make sure
+your syntax is correct, you have the header file downloaded, and your file
+is comiled with no errors. Any other problems just ask me.
+*/
