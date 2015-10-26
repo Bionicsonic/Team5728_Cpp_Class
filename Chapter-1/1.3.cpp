8a4a9e716f568e86fa502166c219fc9ecd521311
Cole Hudson <Cole-Hudson@users.noreply.github.com>
2015-10-26 14:25:30 -0700

    Create 1.3.cpp

---
+++ Chapter-1/1.3.cpp
@@ -0,0 +1,12 @@
+//1.3 Comments
+/*
+Im doing comments right now! As you can see using the '//' indicates to 
+a program, that the particular line is commented out. Whereas using the
+'/ *' and '* /' as shown above and below this comment will comment out
+anything inbetween those two points. These are called delimiters.
+
+You will want to commment on each line of the code you make after a day
+of coding. This will allow you to go back and review everything so you
+understand what you code is doing and can easily understand it when you
+or someone else looks at it again
+*/ 
