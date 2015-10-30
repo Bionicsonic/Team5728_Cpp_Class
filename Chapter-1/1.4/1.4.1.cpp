3561fbd9e9c6c759b868f0f0997b5df595cea9d7
Cole Hudson <Cole-Hudson@users.noreply.github.com>
2015-10-30 00:09:54 -0700

    Update 1.4.1.cpp

--- Chapter-1/1.4/1.4.1.cpp
+++ Chapter-1/1.4/1.4.1.cpp
@@ -23,6 +23,16 @@ int main(){
 	return 0; //Tells compiler all is well
 }
 
+/*
+So our code here starts with our two variables, 'sum' holding the value 0 and 'val'
+holding the value one. Then our while loop evaluates the value of 'val' and if it 
+decides that it is in fact 'less than or equal to 10' then it will return true causing
+the code within its curly braces to run. Which add the current value of 'val' to 'sum', 
+and then increment the value of 'val' before the while loop occurs again. After the while
+loop evaluates to false, which will occur when 'val' holds the value 10, the code after
+the loop will run causing it to output the summation of all numbers from 0 to 10 added. 
+*/
+
 // Exercises
 
 //1.9
